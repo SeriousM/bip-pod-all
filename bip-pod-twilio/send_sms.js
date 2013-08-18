7645c2f7ab65b12a1b7e94c7f51b70a75a3e03a3
michael pearson <gravatar@x0rz.com>
2013-08-18 00:16:34 -0400

    stubbed

---
+++ bip-pod-twilio/send_sms.js
@@ -0,0 +1,129 @@
+/**
+ *
+ * The Bipio Twilio Pod.  send_sms action definition
+ * ---------------------------------------------------------------
+ *  Sends a new outgoing SMS Message. If you are sending SMS while your Twilio 
+ *  account is in Trial mode, the "To" phone number must be verified with Twilio.
+ * ---------------------------------------------------------------
+ *
+ * @author Michael Pearson <michael@cloudspark.com.au>
+ * Copyright (c) 2010-2013 CloudSpark pty ltd http://www.cloudspark.com.au
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var dbox = require('dropbox');
+
+function SendSMS(podConfig) {
+    this.name = 'send_sms';
+    this.description = 'Send an SMS message',
+    this.description_long = 'Sends a new outgoing SMS Message. If you are sending SMS while your Twilio account is in Trial mode, the "To" phone number must be verified with Twilio.',    
+    this.trigger = false; 
+    this.singleton = false;    
+    this.podConfig = podConfig;
+}
+
+SendSMS.prototype = {};
+
+SendSMS.prototype.getSchema = function() {
+    return {
+        'config' : { // config schema
+            properties : {
+                'body' : {
+                    type : 'string',
+                    description : 'Default SMS Body'
+                },
+                'to_phone' : {
+                    type : 'string',
+                    description : 'Default destination phone #'
+                },
+                'from_phone' : {
+                    type : 'string',
+                    description : 'From Phone Number',
+                    required : true
+                }
+            }
+        },
+        'exports' : {        
+            properties : {
+                'status' : {
+                    type : 'string',
+                    description : 'SMS Status'
+                },
+                'sid' : {
+                    type : 'string',
+                    description : 'SMS Message Id'
+                },
+                'body' : {
+                    type : 'string',
+                    description : 'SMS Message Body or Error Response Body'
+                },
+                'uri' : {
+                    type : 'string',
+                    description : 'Twilio SMS URI'
+                }
+            }
+        },
+        // todo validator 160 chars max
+        "imports": {        
+            properties : {            
+                'body' : {
+                    type : 'string',
+                    description : 'SMS Body.  Uses channel default if empty'
+                },
+                'to_phone' : {
+                    type : 'string',
+                    description : 'Recipient Phone #.  Uses channel default if empty'
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Invokes (runs) the action.
+ */
+SendSMS.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
+    var exports = {};
+    
+    // @todo via sysimports auth config
+    // should be migrated into
+    var client = new to.RestClient(
+        sysImports.auth.issuer_token.username, 
+        sysImports.auth.issuer_token.password
+    );
+   
+    var to_phone = imports.phone_to || channel.config.to_phone,
+        body = imports.body || channel.config.body;
+
+    if (body && '' !== body && to_phone && '' !== to_phone) {
+        client.sms.messages.create({
+            to:to_phone,
+            from: channel.from_phone,
+            body: body
+        }, function(error, message) {                       
+            
+            exports.sid = message.sid;
+            exports.body = message.body;
+            exports.status = message.status;
+            exports.uri = message.uri;
+
+            next(error, exports);
+        });
+    } else {
+        next(false, exports);
+    } 
+}
+
+// -----------------------------------------------------------------------------
+module.exports = SendSMS;
