4dbead55b3b490f0a71f3c4ab2760a0e10feb380
michael pearson <gravatar@x0rz.com>
2013-08-17 23:20:11 -0400

    anon upload

--- bip-pod-imgur/README.md
+++ bip-pod-imgur/README.md
@@ -1,4 +1,43 @@
 bip-pod-imgur
-=============
+=======
 
-Imgur Pod for Bipio
+Imgur Pod for Bipio.  
+
+## Installation
+
+From bipio server install directory
+
+    npm install bip-pod-imgur
+
+Auto install script will register the pod with the bipio server and add sparse
+configuration to your NODE_ENV environment config ('default.json', staging or production)
+keyed to 'imgur'.
+
+Manually restart the bipio server at your convenience.
+
+## Actions
+
+### send_sms
+
+Sends a new outgoing SMS Message. If you are sending SMS while your Imgur 
+account is in Trial mode, the "To" phone number must be verified with Imgur.
+
+Sample Channel Config :
+
+```
+"action" : "imgur.image_upload_anon",
+```
+
+[Bipio Docs](https://bip.io/docs/pods/imgur)
+
+## License
+
+BipIO is free for non-commercial use - [GPLv3](http://www.gnu.org/copyleft/gpl.html)
+
+Our open source license is the appropriate option if you are creating an open source application under a license compatible with the GNU GPL license v3. 
+
+Bipio may not be used for Commercial purposes by an entity who has not secured a Bipio Commercial OEM License.  To secure a Commercial OEM License for Bipio,
+please [reach us](mailto:enquiries@cloudspark.com.au)
+
+![Cloud Spark](http://www.cloudspark.com.au/cdn/static/img/cs_logo.png "Cloud Spark - Rapid Web Stacks Built Beautifully")
+Copyright (c) 2010-2013  [CloudSpark pty ltd](http://www.cloudspark.com.au)
---
+++ bip-pod-imgur/image_upload_anon.js
@@ -0,0 +1,110 @@
+/**
+ *
+ * The Bipio Twilio Pod.  send_sms action definition
+ * ---------------------------------------------------------------
+ *  Sends a new outgoing SMS Message. If you are sending SMS while your Twilio
+ *  account is in Trial mode, the "To" phone number must be verified with Twilio.
+ * ---------------------------------------------------------------
+ *
+ * @author Michael Pearson <michael@cloudspark.com.au>
+ * Copyright (c) 2010-2013 CloudSpark pty ltd http://www.cloudspark.com.au
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var request = require('request'),
+    fs = require('fs'),
+    https = require('https');
+
+function ImageUploadAnon(podConfig) {
+    this.name = 'image_upload_anon';
+    this.description = 'Imgur',
+    this.description_long = 'Imgur is used to share photos with social networks and online communities, and has the funniest pictures from all over the Internet.',
+    this.trigger = false;
+    this.singleton = false;
+    this.podConfig = podConfig;
+}
+
+ImageUploadAnon.prototype = {};
+
+ImageUploadAnon.prototype.getSchema = function() {
+    return {        
+        'exports' : {
+            properties : {
+                'id' : {
+                    type : 'string',
+                    description : 'Image ID'
+                },
+                'deletehash' : {
+                    type : 'string',
+                    description : 'Unique Deletion Hash'
+                },
+                'link' : {
+                    type : 'string',
+                    description : 'Link to Image'
+                }
+            }
+        },
+        "imports": {        
+            properties : {
+                "title" : {
+                    type : "string",
+                    description : "Image Title"
+                },
+                "description" : {
+                    type : "string",
+                    description : "Image Description"
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Invokes (runs) the action.
+ */
+ImageUploadAnon.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
+    var log = this.$resource.log, exports = {},  numFiles = contentParts._files.length, dirPfx = '';
+
+    if (contentParts._files && numFiles > 0) {
+        for (var i = 0; i < numFiles; i++) {
+            if (/(png|jpg|gif|jpeg|apng|tiff|bmp|pdf|xcf)$/gi.test(contentParts._files[i].type)) {
+                var options = {
+                    url: 'https://api.imgur.com/3/upload',
+                    headers: {
+                        'Authorization': 'Client-ID ' + this.podConfig.issuer_token.username
+                    }
+                };
+
+                var post = request.post(options, function(err, req, body){
+                    try{
+                        next(err, JSON.parse(body).data);
+                    } catch(e){
+                        log(body, channel, 'error');
+                        next(err, body);
+                    }
+                });
+
+                var upload = post.form();
+                upload.append('type', 'file');
+                upload.append('image', fs.createReadStream(contentParts._files[i].localpath));
+            }
+        }
+    } else {
+        // silent passthrough
+        next(false, exports);    
+    }    
+}
+
+// -----------------------------------------------------------------------------
+module.exports = ImageUploadAnon;
