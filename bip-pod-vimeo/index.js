18dac2553a2a46d47416b9a7dfa7e568a2db179e
michael pearson <gravatar@x0rz.com>
2014-06-28 10:45:07 -0400

    Vimeo video upload

---
+++ bip-pod-vimeo/index.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * Vimeo Actions and Content Emitters
+ *
+ * @author Michael Pearson <michael@cloudspark.com.au>
+ * Copyright (c) 2010-2014 CloudSpark pty ltd http://www.cloudspark.com.au
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Pod = require('bip-pod'),
+VimeoAPI = require('vimeo-api').Vimeo,
+Vimeo = new Pod({
+  name : 'vimeo', // pod name (action prefix)
+  description : 'Vimeo', // short description
+  description_long : '<a href="https://vimeo.com">Vimeo</a>.  Your videos will love it here',
+  authType : 'oauth',
+  passportStrategy : require('passport-vimeo-oauth2').Strategy,
+  config : {
+    "oauth": {
+      "clientID" : "",
+      "clientSecret" : ""
+    }
+  }
+});
+
+Vimeo._getClient = function(sysImports) {
+  var podConfig = this.getConfig(),
+    client = new VimeoAPI(podConfig.oauth.clientID, podConfig.oauth.clientSecret);
+
+  client.access_token = sysImports.auth.oauth.token;
+  client.scope = 'public private upload';
+  return client;
+}
+
+
+// Include any actions
+Vimeo.add(require('./upload.js'));
+
+// -----------------------------------------------------------------------------
+module.exports = Vimeo;
---
+++ bip-pod-vimeo/upload.js
@@ -0,0 +1,70 @@
+/**
+ * @author Michael Pearson <michael@cloudspark.com.au>
+ * Copyright (c) 2010-2014 CloudSpark pty ltd http://www.cloudspark.com.au
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function Upload(podConfig) {
+  this.name = 'upload';
+  this.description = 'Upload Video',
+  this.description_long = 'the long description',
+  this.trigger = false;
+  this.singleton = true;
+  this.auto = false;
+  this.podConfig = podConfig;
+}
+
+Upload.prototype = {};
+
+Upload.prototype.getSchema = function() {
+  return {
+    "exports": {
+      "properties" : {
+        "status" : {
+          "type" : "boolean",
+          "description" : "Upload Complete"
+        }
+      }
+    }
+  }
+}
+
+Upload.prototype.invoke = function(imports, channel, sysImports, contentParts, next) { 
+  var client, pod = this.pod, f, transferredBytes = 0;
+
+  if (contentParts._files && contentParts._files.length) {
+    client = pod._getClient(sysImports);
+    
+    for (var i = 0; i < contentParts._files.length; i++) {
+      f = contentParts._files[i];
+      
+      if ('application/octet-stream' === f.type) {
+        transferredBytes += f.size;
+        client.streamingUpload(f.localpath, function(err) {
+          if (err) {
+            next(err.toString());
+          } else {
+            next(false, { status : true }, contentParts, transferredBytes);
+          }
+          
+        });
+      }
+    }
+  }
+  
+}
+
+// -----------------------------------------------------------------------------
+module.exports = Upload;
