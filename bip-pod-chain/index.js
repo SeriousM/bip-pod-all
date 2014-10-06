10628bf281a48eb18ae8131fd077b7f493760bc3
Scott Tuddenham <scott@wot.io>
2014-10-06 16:26:15 +0000

    BIP-45 setup base config

---
+++ bip-pod-chain/index.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * @author Michael Pearson <github@m.bip.io>
+ * Copyright (c) 2010-2014 Michael Pearson https://github.com/mjpearson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Pod = require('bip-pod'),
+ChainNode = require('chain-node'),
+Chain = new Pod({
+    name : 'chain', // pod name (action prefix)
+    title : 'Chain',
+    description : '<a  href="https://chain.com">Chain</a> enables developers to Build bitcoin apps, not block chain infrastructure.',    
+    authType : 'issuer_token',
+    authMap : {
+        username : 'API Key',
+        password : 'Secret Key'
+    }
+});
+
+// Include any actions
+Chain.add(require('./address.js'));
+Chain.add(require('./transaction.js'));
+Chain.add(require('./unspent.js'));
+Chain.add(require('./opreturn.js'));
+
+// -----------------------------------------------------------------------------
+module.exports = Chain;
