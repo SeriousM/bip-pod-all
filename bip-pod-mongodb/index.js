357b1705e9bc95e98160a8920ec759838616c583
Michael Pearson <github@x0rz.com>
2014-10-22 10:17:49 -0400

    fixed create naming, renamed document_json to document (supoprts mixed string/object types)

--- bip-pod-mongodb/index.js
+++ bip-pod-mongodb/index.js
@@ -1,20 +1,20 @@
 /**
- * 
+ *
  * mongodb Actions and Content Emitters
- * 
+ *
  * @author Scott Tuddenham <scott@bip.io>
  * Copyright (c) 2014 WoT.IO http://wot.io
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,12 +24,12 @@ var MongoClient = require('mongodb').MongoClient;
 var Pod = require('bip-pod'),
     mongodb = new Pod({
         name : 'mongodb', // pod name (action prefix)
-        title : 'mongodb', // short description
-        description : 'A pod to connect to mongodb',
+        title : 'MongoDB', // short description
+        description : '<a href="http://www.mongodb.org" target="_blank">MongoDB</a> (from "humongous") is an open-source document database, and the leading NoSQL database.',
         authType : 'issuer_token',
         authMap : {
             username : 'MongoDB Connection URI'
-        } 
+        }
     });
 
 
@@ -52,24 +52,23 @@ var struct = {
 */
 
 mongodb.testCredentials = function(struct, next) {
-    console.log(struct);
-
     MongoClient.connect(struct.username, function(err, db) {
-            if ( err === null ) {
-                next("OK",200);
-            } else if (err.match(/authenticate/g).length) {
-                next("Unauthorized", 401);
-              } else if (err.match(/connection/g).length) {
-                next("Not Found",404);
-              } else {
-                next("DNS Failure",502); // that or its a replicaset failure.
-              }
+      console.log(arguments);
+      if ( err === null ) {
+        next();
+      } else if (err.match(/authenticate/g).length) {
+        next("Unauthorized", 401);
+      } else if (err.match(/connection/g).length) {
+        next("Not Found",404);
+      } else {
+        next("DNS Failure",502); // that or its a replicaset failure.
+      }
     });
 
 }
 
 // Include any actions
-mongodb.add(require('./insert.js'));
+mongodb.add(require('./create.js'));
 mongodb.add(require('./find.js'));
 mongodb.add(require('./update.js'));
 mongodb.add(require('./delete.js'));
