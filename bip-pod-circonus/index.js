3b16f8f8c383bfff7e025af7d6d4d660278267c7
Michael Pearson <github@x0rz.com>
2014-10-30 12:11:00 -0400

    circonus alert tracker

---
+++ bip-pod-circonus/index.js
@@ -0,0 +1,80 @@
+/**
+ *
+ * Circonus Actions and Content Emitters
+ *
+ * @author Michael Pearson <michael@bip.io>
+ * Copyright (c) 2010-2014 WoT.IO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Pod = require('bip-pod'),
+	https = require('https'),
+  Circonus = new Pod({
+    name : 'circonus', // pod name (action prefix)
+    title : 'Circonus', // short description
+    description : '<a href="http://www.circonus.com/" target="_blank">Circonus</a> monitors and analyzes all your data, from any source',
+    authType : 'issuer_token',
+    authMap : {
+        username : 'App Name',
+        password : 'API Token'
+    },
+	  trackDuplicates : true
+	});
+
+var apiBaseURL = 'https://api.circonus.com/v2'
+
+Circonus.apiGetRequest = function(path, sysImports, next) {
+  this.$resource._httpGet(
+  	apiBaseURL + path,
+  	next,
+		{
+			"Accept" : "application/json",
+      "Authorization" : 'Basic '
+      	+ new Buffer(
+      		sysImports.auth.issuer_token.username
+      		+ ':'
+      		+ sysImports.auth.issuer_token.password
+    		).toString('base64')
+    }
+	);
+}
+
+Circonus.testCredentials = function(struct, next) {
+  this.apiGetRequest(
+  	'/user/current',
+  	{
+  		auth : {
+  			issuer_token : {
+  				username : struct.username,
+  				password : struct.password
+  			}
+  		}
+  	},
+  	function(err, resp, headers, statusCode) {
+    	if (err) {
+    		next(err, 500);
+    	} else if (200 !== statusCode) {
+    		next(resp.message, statusCode);
+    	} else {
+    		next();
+    	}
+    }
+  );
+}
+
+// Include any actions
+Circonus.add(require('./on_new_alert.js'));
+
+// -----------------------------------------------------------------------------
+module.exports = Circonus;
---
+++ bip-pod-circonus/on_new_alert.js
@@ -0,0 +1,105 @@
+/**
+ *
+ * @author Michael Pearson <michael@bip.io>
+ * Copyright (c) 2010-2014 WoT.IO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function OnNewAlert(podConfig) {
+  this.name = 'on_new_alert';
+  this.title = 'On a New Alert';
+  this.description = 'Triggers On a New Alert';
+  this.trigger = false;
+  this.singleton = false;
+  this.auto = false;
+  this.podConfig = podConfig;
+}
+
+OnNewAlert.prototype = {};
+
+OnNewAlert.prototype.getSchema = function() {
+  return {
+    "config": {
+      "properties" : {
+      }
+    },
+    "imports": {
+      "properties" : {
+      }
+    },
+    "exports": {
+      "properties" : {
+        "_value" : {
+          "type" : "string",
+          "description" : "Alert Value"
+        },
+        "_alert_url" : {
+          "type" : "string",
+          "description" : "Alert URL"
+        },
+        "_metric_name" : {
+          "type" : "string",
+          "description" : "Metric Name"
+        },
+        "_metric_notes" : {
+          "type" : "string",
+          "description" : "Metric Notes"
+        },
+        "_metric_link" : {
+          "type" : "string",
+          "description" : "Metric Link"
+        },
+        "_severity" : {
+          "type" : "string",
+          "description" : "Severity"
+        },
+        "_cid" : {
+          "type" : "string",
+          "description" : "Alert ID"
+        },
+        "_occurred_on" : {
+          "type" : "string",
+          "description" : "UTC Unix Time"
+        }
+      }
+    }
+  }
+}
+
+OnNewAlert.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
+  var $resource = this.$resource;
+
+  this.pod.apiGetRequest('/alert', sysImports, function(err, res, headers, statusCode) {
+    if (err || 200 !== statusCode) {
+      next(err || res.message);
+    } else if (app.helper.isArray(res)) {
+      for (var i = 0; i < res.length; i++) {
+        $resource.dupFilter(res[i], '_cid', channel, sysImports, function(err, alert) {
+          if (err) {
+            next(err);
+          } else {
+            console.log(alert);
+            next(false, alert);
+          }
+        });
+      }
+    } else {
+      next('Malformed Response ' + res);
+    }
+  });
+}
+
+// -----------------------------------------------------------------------------
+module.exports = OnNewAlert;
