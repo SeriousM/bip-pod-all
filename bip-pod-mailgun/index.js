c95fb111fa2f654accb9f4129e505a1aaebbbe99
Michael Pearson <github@x0rz.com>
2014-11-04 12:26:54 -0500

    mailgun - send

---
+++ bip-pod-mailgun/index.js
@@ -0,0 +1,60 @@
+/**
+ *
+ * mailgun Actions and Content Emitters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Pod = require('bip-pod'),
+  Client = require('mailgun-js'),
+  MailGun = new Pod({
+    name : 'mailgun', // pod name (action prefix)
+    title : 'MailGun', // short description
+    description : '<a href="https://mailgun.com" target="_blank">MailGun</a> - Powerful APIs that allow you to send, receive and track email effortlessly. Send 10,000 emails for free every month',
+    authType : 'issuer_token',
+    authMap : {
+      password : 'API Key'
+    },
+    "renderers" : {
+      'get_domains' : {
+        description : 'Get Domains',
+        contentType : DEFS.CONTENTTYPE_JSON
+      }
+    }
+  });
+
+var apiBaseURL = 'https://api.mailgun.net/v2';
+
+MailGun.getClient = function(sysImports, domain) {
+  return new Client({
+    apiKey : sysImports.auth.issuer_token.password,
+    domain : domain
+  });
+
+}
+
+// Include any actions
+MailGun.add(require('./send.js'));
+
+MailGun.rpc = function(action, method, sysImports, options, channel, req, res) {
+  if (method == 'get_domains') {
+    this.getClient(sysImports).request('GET', '/domains', {}, function(err, results) {
+      res.status(err ? 500 : 200).send(results);
+    });
+  } else {
+    this.__proto__.rpc.apply(this, arguments);
+  }
+}
+
+// -----------------------------------------------------------------------------
+module.exports = MailGun;
---
+++ bip-pod-mailgun/send.js
@@ -0,0 +1,114 @@
+/**
+ *
+ * The Bipio Send Pod.  mailgun sample action definition
+ * ---------------------------------------------------------------
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// @see http://documentation.mailgun.com/api-sending.html#sending
+
+function Send(podConfig) {
+  this.name = 'send';
+  this.title = 'Send an Email';
+  this.description = 'Send an Email';
+  this.trigger = false;
+  this.singleton = false;
+  this.auto = false;
+  this.podConfig = podConfig;
+}
+
+Send.prototype = {};
+
+Send.prototype.getSchema = function() {
+  return {
+    "config": {
+      "properties" : {
+        "from" : {
+          "type" :  "string",
+          "description" : "Default From Address"
+        },
+        "domain" : {
+          "type" : "string",
+          "description" : "Domain",
+          "oneOf" : [
+            {
+              "$ref" : '/renderers/get_domains#items/{name}'
+            }
+          ],
+          "label" : {
+            "$ref" : "/renderers/get_domains#items/{name}"
+          }
+        }
+      }
+    },
+    "imports": {
+      "properties" : {
+        "from" : {
+          "type" :  "string",
+          "description" : "From Address"
+        },
+        "to" : {
+          "type" :  "string",
+          "description" : "To Address"
+        },
+        "cc" : {
+          "type" :  "string",
+          "description" : "Cc"
+        },
+        "bcc" : {
+          "type" :  "string",
+          "description" : "Bcc"
+        },
+        "subject" : {
+          "type" :  "string",
+          "description" : "Subject"
+        },
+        "text" : {
+          "type" :  "string",
+          "description" : "Text"
+        },
+        "html" : {
+          "type" :  "string",
+          "description" : "HTML"
+        }
+      },
+      "required" : [ "from", "to" ]
+    },
+    "exports": {
+      "properties" : {
+        "message" : {
+          "type" :  "string",
+          "description" : "Queue Message"
+        },
+        "id" : {
+          "type" :  "string",
+          "description" : "Message ID"
+        }
+      }
+    }
+  }
+}
+
+Send.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
+  this.pod.getClient(sysImports, channel.config.domain)
+    .messages()
+    .send(imports, function (err, body) {
+      next(err, body);
+    });
+}
+
+// -----------------------------------------------------------------------------
+module.exports = Send;
