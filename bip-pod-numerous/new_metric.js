3dad5e63080eb5769fc9d19fee66f30251a37ce4
Michael Pearson <github@x0rz.com>
2014-11-07 10:15:41 -0500

    using pod file resource

--- bip-pod-numerous/new_metric.js
+++ bip-pod-numerous/new_metric.js
@@ -19,7 +19,6 @@
 
 var  FormData = require('form-data'),
 https = require('https');
-fs = require('fs');
 
 function NewMetric(podConfig) {
   this.name = 'new_metric';
@@ -101,103 +100,112 @@ NewMetric.prototype.getSchema = function() {
 NewMetric.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
   var log = this.$resource.log,
     postReq = this.$resource._httpPost,
+    $resource = this.$resource,
     schema = this.getSchema(),
-  self = this,
-  form = new FormData(),
-  hasImage = false,
-  // host = "numerous.apiary-mock.com", // production mock
-  //host = "numerous.apiary-proxy.com", // debug proxy mock
-  host = "api.numerousapp.com",
-  requestUrl = '/v1/metrics',
-  file;
+    self = this,
+    form = new FormData(),
+    imageFile,
+    // host = "numerous.apiary-mock.com", // production mock
+    //host = "numerous.apiary-proxy.com", // debug proxy mock
+    host = "api.numerousapp.com",
+    requestUrl = '/v1/metrics',
+    file;
 
-  if (imports.label) {
-    if (contentParts._files && contentParts._files.length) {
-      for (var i = 0; i < contentParts._files.length; i++) {
-        file = contentParts._files[i];
-        if (0 === file.type.indexOf('image')) {
-          form.append('image', fs.createReadStream(file.localpath));
-          hasImage = true;
-          break;
-        }
+
+  // attach the first image file we find
+  if (contentParts._files && contentParts._files.length) {
+    for (var i = 0; i < contentParts._files.length; i++) {
+      file = contentParts._files[i];
+      if (0 === file.type.indexOf('image')) {
+        imageFile = file;
+        break;
       }
     }
-
-    var params = [],
-    p = {};
-    for (var k in imports) {
-      if (imports.hasOwnProperty(k)) {
-        if (imports[k]) {
-          if (schema.imports.properties && schema.imports.properties[k] && 'integer' === schema.imports.properties[k].type) {
-            imports[k] = Number(imports[k]);
-          }
-          p[k] = imports[k];
-        }
-      }
-    }
-
-    for (var k in channel.config) {
-      if (channel.config.hasOwnProperty(k)) {
-        if ('currencySymbol' === k && 'currency' !== channel.config.kind) {
-          continue;
-        }
-        if (imports[k]) {
-          p[k] = channel.config[k];
-        }
-      }
-    }
-
-    postReq('https://api.numerousapp.com/v1/metrics', p, function(err, exports) {
-      if (err) {
-        next(err);
-      } else {
-        if (hasImage) {
-          var options = {
-            protocol : 'https:',
-            host: host,
-            path: '/v1/metrics/' + exports.id + '/photo',
-            auth : sysImports.auth.issuer_token.username + ':'
-          };
-
-          //Do POST request, callback for response
-          var request = form.submit(options, function (err, res) {
-            var body = '';
-
-            if (err) {
-              next(err);
-
-            } else {
-              if (res.statusCode !== 201) {
-                if (res.statusCode === 401) {
-                  next('Invalid API key');
-                } else {
-                  next('HTTP Error '  + res.statusCode);
-                }
-              } else {
-                res.on('data', function(data){
-                  body += data.toString();
-                });
-
-                res.on('end', function(){
-                  try {
-                    next(false, JSON.parse(body));
-                  } catch (e) {
-                    next(e.message);
-                  }
-                });
-              }
-            }
-
-            res.resume();
-          });
-        } else {
-          next(false, exports);
-        }
-      }
-    }, {
-      'Authorization' : 'Basic ' + new Buffer(sysImports.auth.issuer_token.username + ':').toString('base64')
-    });
   }
+
+  var params = [],
+  p = {};
+  for (var k in imports) {
+    if (imports.hasOwnProperty(k)) {
+      if (imports[k]) {
+        if (schema.imports.properties && schema.imports.properties[k] && 'integer' === schema.imports.properties[k].type) {
+          imports[k] = Number(imports[k]);
+        }
+        p[k] = imports[k];
+      }
+    }
+  }
+
+  for (var k in channel.config) {
+    if (channel.config.hasOwnProperty(k)) {
+      if ('currencySymbol' === k && 'currency' !== channel.config.kind) {
+        continue;
+      }
+      if (imports[k]) {
+        p[k] = channel.config[k];
+      }
+    }
+  }
+
+  postReq('https://api.numerousapp.com/v1/metrics', p, function(err, exports) {
+    if (err) {
+      next(err);
+    } else {
+      if (imageFile) {
+        var options = {
+          protocol : 'https:',
+          host: host,
+          path: '/v1/metrics/' + exports.id + '/photo',
+          auth : sysImports.auth.issuer_token.username + ':'
+        };
+
+        $resource.file.get(imageFile, function(err, fileStruct, stream) {
+          if (err) {
+            next(err);
+          } else {
+            //Do POST request, callback for response
+            form.append('image', stream);
+            var request = form.submit(options, function (err, res) {
+              var body = '';
+
+              if (err) {
+                next(err);
+
+              } else {
+                if (res.statusCode !== 201) {
+                  if (res.statusCode === 401) {
+                    next('Invalid API key');
+                  } else {
+                    next('HTTP Error '  + res.statusCode);
+                  }
+                } else {
+                  res.on('data', function(data){
+                    body += data.toString();
+                  });
+
+                  res.on('end', function(){
+                    try {
+                      next(false, JSON.parse(body));
+                    } catch (e) {
+                      next(e.message);
+                    }
+                  });
+                }
+              }
+
+              res.resume();
+            });
+          }
+        });
+
+      } else {
+        next(false, exports);
+      }
+    }
+  }, {
+    'Authorization' : 'Basic ' + new Buffer(sysImports.auth.issuer_token.username + ':').toString('base64')
+  });
+
 }
 
 // -----------------------------------------------------------------------------
