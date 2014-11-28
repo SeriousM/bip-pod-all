568e7e8a091903d0545e5bbe796965c8b49cc8d2
Michael Pearson <github@x0rz.com>
2014-11-28 11:43:18 -0500

    added copyright

--- bip-pod-coindesk/current_price.js
+++ bip-pod-coindesk/current_price.js
@@ -1,4 +1,6 @@
 /**
+ * @author Michael Pearson <michael@bip.io>
+ * Copyright (c) 2010-2014 WoT.IO
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
