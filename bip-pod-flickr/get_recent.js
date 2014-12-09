00e67ab061ed19f71ba4e96f103f1a0b8d72e858
Michael Pearson <github@x0rz.com>
2014-12-09 17:10:41 -0500

    trigger refactor

--- bip-pod-flickr/get_recent.js
+++ bip-pod-flickr/get_recent.js
@@ -32,100 +32,108 @@ GetRecent.prototype.teardown = function(channel, accountInfo, next) {
   this.pod.trackingRemove(channel, accountInfo, next);
 }
 
-GetRecent.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
-  var pod = this.pod,
-    self = this,
-    log = this.$resource.log,
-    mime = this.$resource.mime,
-    userID = (sysImports.auth.oauth.user_id || JSON.parse(sysImports.auth.oauth.profile).id);
-
+GetRecent.prototype.trigger = function(imports, channel, sysImports, contentParts, next) {
+  var self = this;
   pod.trackingGet(channel, function(err, since) {
-    if (!err) {
+    if (err) {
+      next(err);
+    } else {
       pod.trackingUpdate(channel, function(err, until) {
-        pod.getClient(sysImports, function(err, client) {
-          if (err) {
-            log(err, channel, 'error');
-          } else {
-            var args = {
-              user_id : userID,
-              min_upload_date : Math.floor(since / 1000),
-              //extras : 'date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o'
-              extras : 'date_upload, date_taken, owner_name, original_format, last_update, geo, tags, o_dims, views, media, url_m, url_o',
-              page : 1,
-              per_page: 1
-            };
-
-            function getPhotos(args) {
-              client.people.getPhotos(args, function(err, result) {
-                var p;
-                if (err) {
-                  log(err, channel, 'error');
-                } else {
-                  for (var i = 0; i < result.photos.photo.length; i++) {
-                    p = result.photos.photo[i];
-                    if (channel.config.download) {
-                      (function(p) {
-                        pod.getDataDir(channel, self.name, function(err, dataDir) {
-                          if (err) {
-                            next(err);
-                          } else {
-                            var fName = p.url_o.split('/').pop(),
-                              outfile = dataDir + fName,
-                              exports = {
-                                title : p.title,
-                                media_original_url : p.url_o,
-                                tags : p.tags
-                              };
-                            pod._httpStreamToFile(
-                              p.url_o,
-                              outfile,
-                              function(err, exports, fileStruct) {
-                                if (err) {
-                                  next(err);
-                                } else {
-                                  next(err, exports, {
-                                    _files : [ fileStruct ]
-                                    }, fileStruct.size);
-                                }
-                              },
-                              exports,  // export
-                              {       // file meta container
-                                txId : sysImports.id,
-                                localpath : outfile,
-                                name : fName,
-                                type : mime.lookup(fName),
-                                encoding : 'binary'
-                              }
-                            );
-                          }
-                        });
-                      })(p);
-
-                    } else {
-                      next(false, {
-                        title : p.title,
-                        media_original_url : p.url_o,
-                        tags : p.tags
-                      });
-                    }
-                  }
-
-                  if (result.photos.pages) {
-                    if (result.photos.page < result.photos.pages) {
-                      args.page++;
-                      getPhotos(args);
-                    }
-                  }
-                }
-              });
-            }
-
-            getPhotos(args);
-          }
-        });
+        if (!imports.since) {
+          imports.min_upload_date = Math.floor(since / 1000);
+        }
+        self.invoke(imports, channel, sysImports, contentParts, next);
       });
     }
   });
+}
+
+GetRecent.prototype.invoke = function(imports, channel, sysImports, contentParts, next) {
+  var pod = this.pod,
+    self = this,
+    mime = this.$resource.mime,
+    userID = (sysImports.auth.oauth.user_id || JSON.parse(sysImports.auth.oauth.profile).id);
+
+  var args = {
+    user_id : userID,
+    //extras : 'date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o'
+    extras : 'date_upload, date_taken, owner_name, original_format, last_update, geo, tags, o_dims, views, media, url_m, url_o',
+    page : 1,
+    per_page: 1
+  };
+
+  if (imports.min_upload_date) {
+    args.min_upload_date = imports.min_upload_date;
+  }
+
+  pod.getClient(sysImports, function(err, client) {
+    if (err) {
+      next(err);
+    } else {
+      client.people.getPhotos(args, function(err, result) {
+        var photo;
+        if (err) {
+          next(err);
+        } else {
+          for (var i = 0; i < result.photos.photo.length; i++) {
+            photo = result.photos.photo[i];
+            photo.media_original_url = p.url_o;
+
+            // @todo deprecate option
+            if (channel.config.download) {
+
+              (function(photo) {
+                pod.getDataDir(channel, self.name, function(err, dataDir) {
+                  if (err) {
+                    next(err);
+                  } else {
+                    var fName = p.url_o.split('/').pop(),
+                      outfile = dataDir + fName,
+                      exports = photo;
+
+                    pod._httpStreamToFile(
+                      p.url_o,
+                      outfile,
+                      function(err, exports, fileStruct) {
+                        if (err) {
+                          next(err);
+                        } else {
+                          next(err, exports, {
+                            _files : [ fileStruct ]
+                            }, fileStruct.size);
+                        }
+                      },
+                      exports,  // export
+                      {       // file meta container
+                        txId : sysImports.id,
+                        localpath : outfile,
+                        name : fName,
+                        type : mime.lookup(fName),
+                        encoding : 'binary'
+                      }
+                    );
+                  }
+                });
+
+              })(photo);
+
+            } else {
+              next(false, photo);
+            }
+          }
+
+          if (result.photos.pages) {
+            if (result.photos.page < result.photos.pages) {
+              args.page++;
+              getPhotos(args);
+            }
+          }
+        }
+      });
+    }
+  });
+
+
 }
 
 // -----------------------------------------------------------------------------
